d39792170a160fbf7e7afd9807f92eccc0ebc273
Abbos-Amirov <abbosamirov571@gmail.com>
2025-08-10 22:34:46 +0900

    feat: mongodb connection

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import path from 'path';
+
+
+// 1- ENTEANCE
+
+const app = express();
+app.use(express.static(path.join (__dirname,"public")));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// 2 - SESSIONS 
+
+
+
+// 3 -- VIEWS
+
+app.set("views",path.join(__dirname, "views"));///Users/amirovabbos2022/Desktop/Screenshot 2025-07-15 at 21.58.25.png
+app.set("view engine", "ejs");
+
+
+// 4 -- ROUTERS
+
+// app.use("/admin",routerAdmin)
+// app.use("/",router);// midddelware disain pattern
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,56 @@
+
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+
+import app from './app'
+
+mongoose.connect(process.env.MONGO_URL as string,{})
+.then((data)=> {
+    console.log("Mongodb muoffaqiyatli uladi")
+    const PORT = process.env.PORT ?? 3008;
+    console.log("Hammsi joida")
+    app.listen(PORT, function() {
+        console.info( `The server is rumnning successfully on port ${PORT}`)
+        console.info( `Admin project on http://localhost:${PORT}/admin \n`)
+        
+    })
+})
+.catch((xato) => console.log("Error on connection Mongsodb"));
+
+
+
+
+
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT
+
+
+
+// console.log("PORT:",process.env.PORT )
+// console.log("MONGO_URL:",process.env.MONGO_URL )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import moment from "moment";
+
+// const hozirgiVaqt = moment().format("YYYY MM DD");
+// console.log(hozirgiVaqt);
+
+// const person:string ="Oscar";
+// const count:number =77;
+
+// // Architectural pattern: MVS, Dependency Injection, MVP  => Architectural patter nima bu backendning malumiotalr oqimini tartibga soldigan vosita yoki qovurgasi
+
+
+// // Desin patter: Midelleware,Decotar => Desin patten nima =>malumotlar oqimini malom bir qismini tartibga solishda qo'llaniladign pattern hisoblanadi
